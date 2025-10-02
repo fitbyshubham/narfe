7468e414abfb57fdd159e7f2a7af89442b0c447c
shubham <shubhamshrm02@gmail.com>
2025-10-02 16:05:49 +0530

    commented

--- src/app/(pages)/(public)/(home)/page.jsx
+++ src/app/(pages)/(public)/(home)/page.jsx
@@ -2,13 +2,12 @@
 import ModernNavbar from '@/components/layout/ModernNavbar';
 import Hero from '../../(protected)/landing/agency/components/Hero';
 import { blogPosts, jobDetails, projects, services } from '../../(protected)/landing/agency/data';
-import Clients from '../../(protected)/landing/agency/components/Clients';
+//import Clients from '../../(protected)/landing/agency/components/Clients';
 //import Blogs from '../../(protected)/landing/agency/components/Blogs';
-import Features2 from '../../(protected)/landing/app/components/Features2';
+//import Features2 from '../../(protected)/landing/app/components/Features2';
 import EarlyAccessSection from '../../(protected)/landing/agency/components/EarlyAccessSection';
-import RegulatedGradeData from '../../(protected)/landing/coworking/components/RegulatedGradeData';
+//import RegulatedGradeData from '../../(protected)/landing/coworking/components/RegulatedGradeData';
 import CreatorApplicationForm from '../../(protected)/landing/coworking/components/CreatorApplicationForm';
-import Footer from '../../(protected)/landing/agency/components/Footer';
 import NarfeTripPreview from '../../(protected)/landing/agency/components/NarfeTripPreview';
 export const metadata = {
   title: 'Home'
